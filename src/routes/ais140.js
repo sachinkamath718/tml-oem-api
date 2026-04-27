50e9b693f2cccee9203f4a13f6a2b866ba468fcb
Sneha Ratagal <sneharatagal7@gmail.com>
2026-04-27 13:28:43 +0530

    refactor: trim to 5 core APIs, update Postman collection

--- src/routes/ais140.js
+++ src/routes/ais140.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router  = express.Router();
-const authenticate                                  = require('../middleware/auth');
-const { createAIS140Request, getAIS140TicketStatus } = require('../controllers/ais140Controller');
+const authenticate                = require('../middleware/auth');
+const { createAIS140Request }     = require('../controllers/ais140Controller');
 
-router.post('/',              authenticate, createAIS140Request);
-router.post('/ticket-status', authenticate, getAIS140TicketStatus);
+router.post('/', authenticate, createAIS140Request);
 
 module.exports = router;
+
--- src/routes/mining.js
+++ src/routes/mining.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router  = express.Router();
-const authenticate                                    = require('../middleware/auth');
-const { createMiningRequest, getMiningTicketStatus }  = require('../controllers/miningController');
+const authenticate                = require('../middleware/auth');
+const { createMiningRequest }     = require('../controllers/miningController');
 
-router.post('/',              authenticate, createMiningRequest);
-router.post('/ticket-status', authenticate, getMiningTicketStatus);
+router.post('/', authenticate, createMiningRequest);
 
 module.exports = router;
+
